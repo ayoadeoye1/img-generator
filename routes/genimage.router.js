158bf3cc1ada8cddc876949830b84f7f76532555
ayoadeoye1 <isadeoye02@gmail.com>
2023-02-24 14:40:33 +0100

    text-davinci-integrated

--- routes/genimage.router.js
+++ routes/genimage.router.js
@@ -1,6 +1,8 @@
 import  express from "express";
 import { OpenAIApi, Configuration } from "openai";
 
+import { traffic } from "../models/traffic.js";
+
 const router = express.Router();
 
 router.post('/genimage', async(req, res) =>{
@@ -16,12 +18,46 @@ router.post('/genimage', async(req, res) =>{
             size: '512x512',
         });
 
+        if(imgurl) traffic.imageSuccess ++;
+
         const imgurl = imgData.data.data[0].url;
         return res.status(200).json(imgurl);
     } catch (error) {
         console.log(error.message)
         res.status(400).json(error.message);
     }
+})
+
+router.post('/gentext', async(req, res) =>{
+    try {
+        const body = req.body;
+        const text = Object.keys(body)[0];
+
+        const configuration = new Configuration({apiKey: process.env.APIKEY});
+        const openai = new OpenAIApi(configuration)
+        const resText = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: text,
+            max_tokens: 256,
+            temperature: 0.7,
+            top_p: 1,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+        });
+
+        if(resText) traffic.textSuccess ++;
+
+        const textResp = resText.data.choices[0].text;
+
+        return res.status(200).json(textResp);
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).json(error.message);
+    }
+})
+
+router.get('/traffic', async(req, res) =>{
+    await res.json(traffic);
 })
 
 export default router;
